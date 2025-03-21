de00abf1e9585aac292b1ec914c59d69d9bdb50b
joao3313 <ikkijp2012@gmail.com>
2025-03-20 21:24:45 -0300

    Adicionar arquivos de projeto.

---
+++ Multiplicaçao/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Multiplicacao
+{
+    using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            int numero1, numero2;
+
+            // Solicita e valida o primeiro número
+            while (true)
+            {
+                Console.Write("Digite o primeiro número: ");
+                if (int.TryParse(Console.ReadLine(), out numero1))
+                    break;
+                else
+                    Console.WriteLine("Entrada inválida! Por favor, insira um número inteiro.");
+            }
+
+            // Solicita e valida o segundo número
+            while (true)
+            {
+                Console.Write("Digite o segundo número: ");
+                if (int.TryParse(Console.ReadLine(), out numero2))
+                    break;
+                else
+                    Console.WriteLine("Entrada inválida! Por favor, insira um número inteiro.");
+            }
+
+            // Calcula e exibe a multiplicação
+            double multiplicacao = (double)numero1 * numero2;
+            Console.WriteLine($"A multiplicação de {numero1} por {numero2} é: {multiplicacao:F2}");
+        }
+    }
+}
+
